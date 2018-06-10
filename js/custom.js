5dfdde1d5056c38d2c1271ab9dcbfdadbe9e326e
Elizarov Andrei <elizarov13@yandex.ru>
2018-06-11 00:16:35 +0300

    табуляция

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,28 @@
+// $(function(){
+
+// 	var hf = function(){
+// 		var h_header = $('header').height();
+// 		var h_footer = $('footer').height();
+// 		$('.content').css({
+// 			'paddingTop': h_header,
+// 			'paddingBottom': h_footer
+// 		});
+// 	}
+
+// 	$(window).bind('load resize', hf);
+
+// });
+
 $(function(){
-
-	var hf = function(){
-		var h_header = $('header').height();
-		var h_footer = $('footer').height();
-		$('.content').css({
-			'paddingTop': h_header,
-			'paddingBottom': h_footer
-		});
-	}
-
-	$(window).bind('load resize', hf);
-
+	$(".tabs a").click(function(){
+		$(this).parents(".tab-wrap").find(".tab-cont").addClass("hide");
+		$(this).parents().siblings().removeClass("active");
+		var id=$(this).attr("href");
+		$(id).removeClass("hide");
+		$(this).parent().addClass("active");
+		return false
+	});
 });
-
    
 
 
