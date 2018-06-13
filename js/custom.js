33b0e8fe01b51810da258f416d738f18e2fe9ebb
Elizarov Andrei <elizarov13@yandex.ru>
2018-06-14 00:01:11 +0300

    slider

--- js/custom.js
+++ js/custom.js
@@ -22,7 +22,18 @@ $(function(){
 		$(this).parent().addClass("active");
 		return false
 	});
+
+	$(".banner-slider, .testimonial-slider").slick({
+		arrows: false,
+		dots: true,
+	});
+	$(".portfolio-slider").slick({
+		dots: true,
+		appendArrows: ".portfolio-slider__buttons",
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+	})
+
 });
    
-
 
