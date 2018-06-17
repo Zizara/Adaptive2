68b0475c319cab9ceacc43e537f606be3843162a
Elizarov Andrei <elizarov13@yandex.ru>
2018-06-18 00:08:12 +0300

    адаптив кнопка вверх

--- js/custom.js
+++ js/custom.js
@@ -12,16 +12,35 @@
 // 	$(window).bind('load resize', hf);
 
 // });
-//табуляция
+
 $(function(){
+
+	//гамбургер
+	$(".menu-toggle").click(function() {
+		//Добавляет или удаляет заданный класс
+		$(this).toggleClass("active")
+		//в течении 600 милисекунды свернет/развернет элемент с идентификатором menu. 
+		$(".menu").slideToggle(600)
+	})
+
+	//табуляция
 	$(".tabs a").click(function(){
+		// в .tabs родитель tab-wrap найти tab-cont и всем добавить hide
 		$(this).parents(".tab-wrap").find(".tab-cont").addClass("hide");
+		//вернет всех предков элемента, имеющего идентификатор tabs a 
+		//найдет элементы, которые имеют общих родителей, с элементами класса tabs a
+		//у найденых эл-ов удалить класс active
 		$(this).parents().siblings().removeClass("active");
-		var id=$(this).attr("href");
+		//в переменную id  записать href элемент
+		//вернет href у первого tabs a элемента 
+		var id = $(this).attr("href");
+		//удалить класс hide у первого href
 		$(id).removeClass("hide");
+		//добавить в tabs а родитель является ul класс active
 		$(this).parent().addClass("active");
 		return false
 	});
+
 //слайдер
 	$(".banner-slider, .testimonial-slider").slick({
 		arrows: false,
@@ -40,10 +59,44 @@ $(function(){
 				}
 			}
 		]
-	})
+	});
 
+//слайдер
+	$nav_tab_slider = $(".nav-tab-list");
+		settings = {
+			slidesToShow: 1,
+			prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+			nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+		}
+		//slickSlider
+	$nav_tab_slider.on("afterChange", function(event, slick, currentSlide, nextSlide){
+		//ищем родителя nav-tab-list найти tab-cont и добавить класс hide
+		$(this).parents(".tab-wrap").find(".tab-cont").addClass("hide");
+		$(this).find(".slick-current").siblings().removeClass("active");
+		//у активного эл-та (slick-current) получаем hgef
+		var id = $(this).find(".slick-current a").attr("href");
+		//у актинвного эл-та убрать класс hide
+		$(id).removeClass("hide");
+		// найти slick-current и добавить класс active
+		$(this).find(".slick-current").addClass("active");
+		return false
+	});
+	//обработчик изменения размеров окна браузера и полной загрузки
+	$(window).on("resize load", function(){
+
+		if($(window).width() > 767) {
+			//проверит наличие класса slick-initialized у элемента  nav-tab-list
+			if($nav_tab_slider.hasClass("slick-initialized")) {
+				//если есть сам слайдер его остановить unslick
+				$nav_tab_slider.slick("unslick")
+			}
+			return
+		}
+		if(!$nav_tab_slider.hasClass("slick-initialized")) {
+			return $nav_tab_slider.slick(settings)
+		}
+	})
 });
-   
 
 function initMap() {
     var coordinates = {lat: -37.806006, lng: 144.961291}, // Координаты центра карты 
